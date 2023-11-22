451831a5479129aef5d5fe960deae9e83314f091
Durlov <durlov05@gmail.com>
2023-11-22 14:34:40 -0700

    Add files via upload

---
+++ PointOnMap.java
@@ -0,0 +1,30 @@
+package socialIsland;
+
+public class PointOnMap {
+	
+	private double x;
+	private double y;
+
+	public PointOnMap(double x, double y) {
+		this.x = x;
+		this.y = y;
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	
+}
