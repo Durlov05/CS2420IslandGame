49902124d4dfcc3aed38d5473ebc100133c812f9
Durlov <durlov05@gmail.com>
2023-11-29 22:57:25 -0700

    Update RemoveEdge.java - Doc Comments

--- CodeBeingUse/RemoveEdge.java
+++ CodeBeingUse/RemoveEdge.java
@@ -1,74 +1,98 @@
+/**
+ * The RemoveEdge class demonstrates a method to remove an edge between two vertices
+ * in a graph represented by the edu.princeton.cs.algs4.Graph class.
+ */
+
 package socialIsland;
 
 import edu.princeton.cs.algs4.Graph;
 import edu.princeton.cs.algs4.StdOut;
 
 public class RemoveEdge {
+
+	/**
+	 * The main method creates a graph, adds edges, displays the original adjacency
+	 * list, removes an edge between two vertices, and displays the modified
+	 * adjacency list.
+	 *
+	 * @param args Command-line arguments (not used)
+	 * 
+	 * @author Eliza Kitchens & Syed Mujibur Rahman (Mujib)
+	 */
+
 	public static void main(String[] args) {
-        Graph G = new Graph(5);
-        G.addEdge(0, 1);
-        G.addEdge(0, 2);
-        G.addEdge(0, 4);
-        G.addEdge(1, 4);
-        G.addEdge(1, 2);
-        StdOut.println("Adjacency List");
-        StdOut.println("--------------");
-        for (int v = 0; v < G.V(); v++) {
-        	boolean first = true;
-        	StdOut.printf("%d: ", v);
-        	for (int adj : G.adj(v)) {
-        		if (!first) {
-        			StdOut.print(" -> ");
-                } else {
-                    first = false;
-                }
-        		StdOut.print(adj);
-        	}
-        	StdOut.println();
-        }
-        
-        StdOut.println();
-        
-        Graph G2 = RemoveEdgeMethod(G, 1, 2);
-        
-        StdOut.println("Adjacency List2");
-        StdOut.println("--------------");
-        for (int v = 0; v < G2.V(); v++) {
-        	boolean first = true;
-        	StdOut.printf("%d: ", v);
-        	for (int adj : G2.adj(v)) {
-        		if (!first) {
-        			StdOut.print(" -> ");
-                } else {
-                    first = false;
-                }
-        		StdOut.print(adj);
-        	}
-        	StdOut.println();
-        }
+		Graph G = new Graph(5);
+		G.addEdge(0, 1);
+		G.addEdge(0, 2);
+		G.addEdge(0, 4);
+		G.addEdge(1, 4);
+		G.addEdge(1, 2);
+		StdOut.println("Adjacency List");
+		StdOut.println("--------------");
+		for (int v = 0; v < G.V(); v++) {
+			boolean first = true;
+			StdOut.printf("%d: ", v);
+			for (int adj : G.adj(v)) {
+				if (!first) {
+					StdOut.print(" -> ");
+				} else {
+					first = false;
+				}
+				StdOut.print(adj);
+			}
+			StdOut.println();
+		}
+
+		StdOut.println();
+
+		Graph G2 = RemoveEdgeMethod(G, 1, 2);
+
+		StdOut.println("Adjacency List2");
+		StdOut.println("--------------");
+		for (int v = 0; v < G2.V(); v++) {
+			boolean first = true;
+			StdOut.printf("%d: ", v);
+			for (int adj : G2.adj(v)) {
+				if (!first) {
+					StdOut.print(" -> ");
+				} else {
+					first = false;
+				}
+				StdOut.print(adj);
+			}
+			StdOut.println();
+		}
 	}
-	
-	public static Graph RemoveEdgeMethod(Graph G, int vertex, int adjacent) {	
-		
+
+	/**
+	 * Removes an edge between two vertices in the given graph.
+	 *
+	 * @param G        The original graph
+	 * @param vertex   One of the vertices forming the edge to be removed
+	 * @param adjacent The other vertex forming the edge to be removed
+	 * @return A new graph with the specified edge removed
+	 */
+
+	public static Graph RemoveEdgeMethod(Graph G, int vertex, int adjacent) {
+
 		int verticeCounter = 0;
-		boolean doublecounting = false; 
-		
+		boolean doublecounting = false;
+
 		for (int v = 0; v < G.V(); v++) {
 			verticeCounter++;
 		}
-		
+
 		Graph G2 = new Graph(verticeCounter);
-		
+
 		for (int v = 0; v < G.V(); v++) {
 			for (int adj : G.adj(v)) {
 				doublecounting = false;
-				if ((adj == vertex && v == adjacent) || (adj == adjacent && v == vertex) ) {
-				// This is used to skip adding vertex and adjacent as an Edge.
+				if ((adj == vertex && v == adjacent) || (adj == adjacent && v == vertex)) {
+					// This is used to skip adding vertex and adjacent as an Edge.
 					continue;
-				}
-				else {
+				} else {
 					for (int adj2 : G2.adj(v)) {
-						if (adj2 == adj) 
+						if (adj2 == adj)
 							doublecounting = true;
 					}
 					if (doublecounting == false)
@@ -77,7 +101,7 @@ public class RemoveEdge {
 			}
 		}
 		return G2;
-		
+
 	}
-	
+
 }
