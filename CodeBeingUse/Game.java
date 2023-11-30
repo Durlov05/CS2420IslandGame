fe6f80ef223a16075746017c183373469f446d6e
Durlov <durlov05@gmail.com>
2023-11-29 22:53:23 -0700

    Update Game.java - Doc Comments and Spacing

--- CodeBeingUse/Game.java
+++ CodeBeingUse/Game.java
@@ -2,34 +2,37 @@ package socialIsland;
 
 import edu.princeton.cs.algs4.Graph;
 
-public class Game implements GameMethods {
-
 /**
- * This is the main Game method that implements GameMethods and calls the 
- * functions to set up the environment and handle gameplay.
+ * The Game class implements GameMethods and orchestrates the setup of the game
+ * environment and handles gameplay mechanics.
  * 
  * @author Eliza Kitchens & Syed Mujibur Rahman (Mujib)
  */
-	
+
+public class Game implements GameMethods {
+
+	/**
+	 * The main method initiates the game by initializing characters, setting up the
+	 * environment, and gameplay.
+	 */
+
 	public static void main(String[] args) {
 
 		Character[] characters = GameMethods.initializeCharacters(6);
 		GameMethods.setEnvironment(characters);
-		double [] xCoordinates = new double[characters.length];
-		double [] yCoordinates = new double[characters.length];
-		for (int i = 1; i <characters.length; i++ ) {
+
+		double[] xCoordinates = new double[characters.length];
+		double[] yCoordinates = new double[characters.length];
+
+		for (int i = 1; i < characters.length; i++) {
 			xCoordinates[i] = characters[i].getxCoordinate();
 			yCoordinates[i] = characters[i].getyCoordinate();
 		}
-	
+
 		Graph g = new Graph(characters.length);
 
 		GameMethods.checkForClicksCharacters(characters, g, xCoordinates, yCoordinates);
-		
-		
 
 	}
 
 }
-
-
