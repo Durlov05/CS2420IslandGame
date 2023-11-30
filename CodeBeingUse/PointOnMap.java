98960a64e69c7b64818b5de20ef7422d2fc254fe
Durlov <durlov05@gmail.com>
2023-11-29 22:57:00 -0700

    Update PointOnMap.java - Doc Commets

--- CodeBeingUse/PointOnMap.java
+++ CodeBeingUse/PointOnMap.java
@@ -1,9 +1,22 @@
 package socialIsland;
 
+/**
+ * The PointOnMap class represents a point on a map with coordinates (x, y).
+ * 
+ * @author Eliza Kitchens & Syed Mujibur Rahman (Mujib)
+ */
+
 public class PointOnMap {
-	
+
 	private double x;
 	private double y;
+
+	/**
+	 * Constructs a PointOnMap object with the specified x and y coordinates.
+	 * 
+	 * @param x The x-coordinate of the point on the map
+	 * @param y The y-coordinate of the point on the map
+	 */
 
 	public PointOnMap(double x, double y) {
 		this.x = x;
@@ -26,5 +39,4 @@ public class PointOnMap {
 		this.y = y;
 	}
 
-	
 }
