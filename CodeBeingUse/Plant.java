d2013c5e9edc2b245b79ad0cbffc1756d5c037c5
wildberryprincess <147541809+wildberryprincess@users.noreply.github.com>
2023-12-02 11:43:21 -0700

    Add files via upload

---
+++ CodeBeingUse/Plant.java
@@ -0,0 +1,37 @@
+package socialIsland;
+
+/**
+ * Class stores information about the plants in the gameplay environment.
+ */
+public class Plant {
+	private double xCoordinate;
+	private double yCoordinate;
+	//private String image;
+	public Plant(double xCoordinate, double yCoordinate) {
+		this.xCoordinate = xCoordinate;
+		this.yCoordinate = yCoordinate;
+	}
+	public double getxCoordinate() {
+		return xCoordinate;
+	}
+	public void setxCoordinate(double xCoordinate) {
+		this.xCoordinate = xCoordinate;
+	}
+	public double getyCoordinate() {
+		return yCoordinate;
+	}
+	public void setyCoordinate(double yCoordinate) {
+		this.yCoordinate = yCoordinate;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+}
